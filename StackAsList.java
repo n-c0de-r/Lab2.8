b2fc75527bd76f188b69ccda8ae872c3ec34af5e
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-06-03 12:10:44 +0200

    Added JDoc header

--- StackAsList.java
+++ StackAsList.java
@@ -1,3 +1,10 @@
+/**
+ * This class takes a Stack and converts it to a Linked List.
+ * 
+ * @author	robinwettstaedt
+ * @author	n-c0de-r
+ * @version	21-06-03 
+ */
 
 //Assignment 2
 public class StackAsList {
@@ -5,4 +12,6 @@ public class StackAsList {
 	//TODO Class body
 	
 	//TODO methods maybe
+	
+	//TODO @Override toString()
 }
