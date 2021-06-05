6e07c4a07fd0925b55f71e28bfe471a81c433972
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-06-06 00:12:46 +0200

    Guards against unwelcome behaviour pop & push, JDoc!
    Popping was done (or at least tried) even on empty stacks, which was wrong as it should be done on encountering an operator, and then perform the calculation on top!
    Also pushing the result should be done after a calculation, not after each run of the loop, which fills the stack with zeros!
    Further descriptions of classes and methods for JDoc generation and more comments on code function.

--- Postfix.java
+++ Postfix.java
@@ -5,18 +5,21 @@
  * 
  * @author	robinwettstaedt
  * @author	n-c0de-r
- * @version	21-06-03 
+ * @version	21-06-03 update 21-06-06
  */
 
 //Assignment 3
 public class Postfix {
 	
-	//TODO Class body
-	
+    /**
+     * Evaluate the value of an expression in Reverse Polish Notation/Postfix
+     * @param pfx 	String containing the expression
+     * @return		Integer value of the given expression
+     */
 	public int evaluate (String pfx) {
 		//variables holding left-hand side and right-hand side of an expression
-		int lhs;
-		int rhs;
+		int lhs = 0;
+		int rhs = 0;
 		
 		Stack s = new StackAsList();
 		
@@ -28,13 +31,18 @@ public class Postfix {
 		
 		//While the array is not looped through
 		int i = 0;
-		while (i < pfx.length()-1) {
+		while (i < pfx.length()) {
+			//Divide the arithmetic expression string and look into the first element.
 			first = pfx.substring(i,i+1);
 			
-			rhs = Integer.parseInt(s.pop());
-			lhs = Integer.parseInt(s.pop());
-			
-			switch (first) {
+			//If it is an operator, take the first two operands from the stack and calculate them accordingly
+			if (first == "+" || first == "-" || first == "*" || first == "/" || first == "^") {
+				//Store the operands in respective sides
+				rhs = Integer.parseInt(s.pop());
+				lhs = Integer.parseInt(s.pop());
+				
+				//Do following calculations depending on found operator
+				switch (first) {
 				case "+":
 					r = lhs + rhs;
 					break;
@@ -50,15 +58,26 @@ public class Postfix {
 				case "^":
 					r = (int) Math.pow(lhs,  rhs);
 					break;
-				default:
-					s.push(first);
 			}
+				//Push the result to the stack
+				s.push(Integer.toString(r));
+			} else {
+				//If it's an operand push it on top of the stack
+				s.push(first);
+			}
+			//When done with the current token, move on
 			i++;
 		}
-		s.push(Integer.toString(r));
+		// When finished return the resulting integer
 		return r;
 	}
 	
+    /**
+     * Convert any standard arithmetic expression from infix
+     * notation to Reverse Polish Notation /Postfix
+     * @param pfx 	String containing the expression to convert
+     * @return 		String of the resulting postfix expression
+     */
 	// Assignment 4
 //	public String infixToPostfix (String ifx){
 //		//TODO method body
