3bffb2a577f67bf454541d8b8736ac06c12fdcea
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-06-06 17:51:27 +0200

    Added user input and result messages

--- Converter.java
+++ Converter.java
@@ -12,13 +12,20 @@
 public class Converter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		//create a new postfix object, that provides means of evaluation and conversion
 		Postfix p = new Postfix();
 		
-		System.out.println(p.evaluate("1 2 * 3 +"));
+		//System.out.println(p.evaluate("1 2 * 3 +")); checking if postfix works, obsolete with tests
 		
-		//Probably a Scanner to take inputs and pass it to be converted?
+		System.out.println("Please enter an infix expression.");
+		System.out.println("Each operand and operator must be separated by spaces!");
+		// Like in Chatterbox Lab
+		Scanner s = new Scanner(System.in);
+		System.out.println();
+		System.out.println("The resulting postfix notation is: " + p.infixToPostfix(s.nextLine().trim()));
+		System.out.println();
+		System.out.println("The result of the postfix expression equals to: " +  + p.evaluate(p.infixToPostfix(s.nextLine().trim())));
+		s.close();
 	}
 
 }
