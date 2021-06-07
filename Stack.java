62afd04614efad6bf3d101993bcbf5240b3448bc
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-06-07 11:10:57 +0200

    put peek and empty back in, needed after all

--- Stack.java
+++ Stack.java
@@ -4,13 +4,13 @@
  * 
  * @author	robinwettstaedt
  * @author	n-c0de-r
- * @version	21-06-03 update 21-06-06
+ * @version	21-06-03 update 21-06-07
  */
 
 //Assignment 1
 public interface Stack {
-    abstract String push(String element);
+    abstract String	push(String element);
     abstract String pop();
-    /*abstract String peak();
-    abstract boolean isEmpty(); not necessary for this lab*/
+    abstract String peek();
+    abstract boolean isEmpty();
 }
